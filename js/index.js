bb7518bdece8e8ae880867aab9c78b30da60f9e1
iRogerz <rogertseng1025@gmail.com>
2020-11-02 15:46:41 +0800

    1.0

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// // if you're using a bundler, first import:
+// import Headroom from "headroom.js";
+// // grab an element
+// var myElement = document.querySelector("header");
+// // construct an instance of Headroom, passing the element
+// var headroom  = new Headroom(myElement);
+// // initialise
+// headroom.init();
+$('.carousel').carousel({
+    interval: 2000
+  })
